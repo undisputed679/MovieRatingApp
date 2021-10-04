98e285648c257facbe66d7b06e1020a1218baa8d
undisputed679 <mayankbhushan679@gmail.com>
2021-10-04 18:50:12 +0530

    created serializer

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+
+
+from django.conf.urls import include
+from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+]
